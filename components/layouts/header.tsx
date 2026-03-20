c72aae4a85936bd8f4d6b4024e279e6fc4ce5763
Pham Ngoc Phong <phongpham131102@gmail.com>
2026-03-20 21:44:24 +0700

    refactor: simplify main page content by removing boilerplate and introducing a new Header component.

---
+++ components/layouts/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="w-full h-16 bg-zinc-50 dark:bg-black fixed top-0 left-0 right-0">
+      <h1>Chat App</h1>
+    </header>
+  );
+}
