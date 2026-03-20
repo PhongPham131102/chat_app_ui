8f46f305be987cdc28e9cfb8b3a2b0bffcfa1541
Pham Ngoc Phong <phongpham131102@gmail.com>
2026-03-20 21:57:04 +0700

    feat: Redesign header with a new logo and login button, introducing a main color variable.

--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+import { Button } from "../ui/button";
+
 export default function Header() {
   return (
-    <header className="w-full h-16 bg-zinc-50 dark:bg-black fixed top-0 left-0 right-0">
-      <h1>Chat App</h1>
+    <header className="w-full h-16 bg-main text-white flex items-center justify-between px-10 fixed top-0 left-0 right-0">
+      <div className="flex items-center gap-4">
+        <Image
+          src="/what-app.svg"
+          alt="What App Logo"
+          width={130}
+          height={40}
+        />
+      </div>
+      <div className="flex items-center gap-4">
+        <Button>Đăng nhập</Button>
+      </div>
     </header>
   );
 }
