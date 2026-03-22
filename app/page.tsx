f0f625f59b56a087fd807afdf3c4b91d94719ebb
Pham Ngoc Phong <phongpham131102@gmail.com>
2026-03-22 22:24:00 +0700

    feat: Add call section to the home page, including its new component and image asset.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
+import CallSection from "@/components/layouts/call-section";
 import ChatHeroSection from "@/components/layouts/chat-hero-section";
 import Header from "@/components/layouts/header";
 import MessageHeroSection from "@/components/layouts/message-hero-section";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center bg-main justify-center  font-sans h-screen w-screen">
+    <div className="flex flex-col flex-1 items-center bg-main justify-center  font-sans h-screen">
       <Header />
-      <main className="flex flex-col w-full h-[200vh] mt-20  items-center justify-start">
+      <main className="flex flex-col w-full  mt-20  items-center justify-start">
         <ChatHeroSection />
         <MessageHeroSection />
+        <CallSection />
       </main>
     </div>
   );
